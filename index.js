d853e05bf9804f0c487f735762e30f319258e6c3
SpikySpike <slimeyggmain@gmail.com>
2021-07-17 18:10:10 +0300

    added 8ball command

--- index.js
+++ index.js
@@ -155,6 +155,33 @@ client.on('message', message => {
         client.commands.get('').execute(message, args);
     } else if (command === 'help'){
         message.channel.send('still being built')
+    } else if (command === '8ball') {
+        if (!args[2]){
+            return message.channel.send("Please ask something!")
+        }
+        let number = Math.floor(Math.random() * 7);
+        if (number == 0){
+            return message.channel.send("Yes, definitely so.")
+        }
+        if (number == 1){
+            return message.channel.send("No, definitely not.")
+        }
+        if (number == 2){
+            return message.channel.send("Ask again later.")
+        }
+        if (number == 3){
+            return message.channel.send("It is uncertain.")
+        }
+        if (number == 4){
+            return message.channel.send("Odds are not in your favor.")
+        }
+        if (number == 5){
+            return message.channel.send("Odds are in your favor.")
+        } 
+        if (number == 6){
+            return message.channel.send("ask ur mom peasant :rofl: :point_right:")
+        }
+
     }
 });
 
