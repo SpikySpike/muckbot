b4449100656c2f923853045d756fe477aa17e346
Carrot <iivo.makinen@yandex.com>
2021-09-12 19:21:50 +0300

    Test

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,7 @@ module.exports = {
         if(!args[0]) return message.reply("Please enter the amount of messages that you want to delete!");
         if(isNaN(args[0])) return message.reply("Please enter a real number!"), message.react("❌");
 
-        if(args[0]> 100) return message.reply("You can't delete more than 100 messages!"), message.react("❌");
+        if(args[0]> 1000) return message.reply("You can't delete more than 1000 messages!"), message.react("❌");
         if(args[0] < 1) return message.reply("You must delete atleast one message!"), message.react("❌");
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
