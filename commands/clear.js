4a88f5e6b77f2d3e0eaec0682abd5373b6ac1020
Carrot <iivo.makinen@yandex.com>
2021-09-12 19:49:21 +0300

    test clear.js
    added clear all

--- commands/clear.js
+++ commands/clear.js
@@ -1,16 +1,25 @@
 module.exports = {
     name: 'clear',
     description: "Clear messages!",
-    async execute(message, args){
-        if(!args[0]) return message.reply("Please enter the amount of messages that you want to delete!");
-        if(isNaN(args[0])) return message.reply("Please enter a real number!"), message.react("❌");
+    async execute(message, args){ 
+        if(args[0] == "all") {
+            for (let i = 0; i < 10; i++) {
+                message.channel.bulkDelete(100);
+                message.reply(`Cleared 1000 messages :wastebasket`);
+            }
+            break;
+        }
+        else {
+            if(!args[0]) return message.reply("Please enter the amount of messages that you want to delete!");
+            if(isNaN(args[0])) return message.reply("Please enter a real number!"), message.react("❌");
 
-        if(args[0]> 100) return message.reply("You can't delete more than 100 messages!"), message.react("❌");
-        if(args[0] < 1) return message.reply("You must delete atleast one message!"), message.react("❌");
+            if(args[0]> 100) return message.reply("You can't delete more than 100 messages!"), message.react("❌");
+            if(args[0] < 1) return message.reply("You must delete atleast one message!"), message.react("❌");
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
             message.channel.bulkDelete(messages);
             message.reply(`cleared ${args} messages! :wastebasket:`);
-        });
+            });
+        }
     }
 }
